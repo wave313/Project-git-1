9699e628218d1764a03dfb14a0b57877c3dfb8c6
bowkittiya <popittt4750@gmail.com>
2025-08-18 13:08:45 +0700

    update

--- classroom-git/src/main.jsx
+++ classroom-git/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css"; // ถ้ามี Tailwind หรือ CSS ปกติ
+import Navbar from "./components/Navbar"; // นำเข้า Navbar ถ้ามี
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
